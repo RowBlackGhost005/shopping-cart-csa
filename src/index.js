f388a83e54d2baffa5fb828f27abde773842465d
Luis Angel Marin <la.marin@outlook.com>
2025-06-20 20:09:07 -0700

    MSW Setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Only start MSW in development
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = await import('./mocks/browser')
+  await worker.start()
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
