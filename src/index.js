aa1baa33657244685ff786ed7fa81d4516d6e564
Luis Angel Marin <la.marin@outlook.com>
2025-06-21 22:34:42 -0700

    MSW Fix

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const enableMocking = async () => {
   }
 };
 
-enableMocking.then(() => {
+enableMocking().then(() => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
